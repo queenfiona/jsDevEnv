bff5d26119b59059613f7fe5d851d1f1fdb4929b
queenfiona <fionaitya@gmail.com>
2019-06-27 21:51:36 +0300

    configure development server using express

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,19 @@
+let express = require("express");
+let path = require('path');
+let open = require('open');
+
+let port = 3000;
+let app = express();
+
+// Declare routing, funcs params(request,response)
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname,'../src/index.html'));
+});
+
+app.listen(port, function(err){
+  if (err) {
+    console.log(err);
+  }else{
+    open('http://localhost:' + port);
+  }
+});
