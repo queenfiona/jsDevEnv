68ecdaf03c2ff13b6d4c9493c41c392fc081d4fe
queenfiona <fionaitya@gmail.com>
2019-07-02 00:33:24 +0300

    serve mock data via json-server

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import {getUsers} from './api/userApi';
 
 getUsers().then(result=>{
+  console.log(result);//eslint-disable-line no-console
   let userBody = "";
 
-  result.forEach(user=>{
-    userBody+= `<tr>
-    <td><a href='#' data-id="${user.id}" class=deleteUser>Delete</a></td>
-    <td>${user.id}</td>
-    <td>${user.firstName}</td>
-    <td>${user.lastName}</td>
-    <td>${user.email}</td>
-    </tr>`
-  });
+  if (result) {
+    result.forEach(user=>{
+      userBody+= `<tr>
+      <td><a href='#' data-id="${user.id}" class=deleteUser>Delete</a></td>
+      <td>${user.id}</td>
+      <td>${user.firstName}</td>
+      <td>${user.lastName}</td>
+      <td>${user.email}</td>
+      </tr>`
+    });
 
-  global.document.getElementById('users').innerHTML = userBody;
+    global.document.getElementById('users').innerHTML = userBody;
+  } else{
+    global.document.getElementById('users').innerText = "No data could be found";
+  }
 });
