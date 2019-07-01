e1ee20ac955d57780328e04ebbe08966247dc016
queenfiona <fionaitya@gmail.com>
2019-07-01 14:10:02 +0300

    set up eslint for linting

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,4 +1,4 @@
 // Message displayed after dev server starts
 import chalk from 'chalk';
 
-console.log(chalk.green('Starting app in dev mode...'));
+console.log(chalk.green('Starting app in dev mode...'));//eslint-disable-line no-console
